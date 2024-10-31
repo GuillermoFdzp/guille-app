50a91c4c6042b7ff7005617a45483e0d11aab570
GuillermoFdzp <guillermo.fernandez.perez2005@gmail.com>
2024-10-31 20:13:26 +0100

    edicion final

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 export default function Inicio() {
   return (
     <div className="py-8">
-      <h1 className="text-3xl font-bold">Sobre mí</h1>
+      <h1 className="text-3xl font-bold text-center">Sobre mí</h1>
       <p className="mt-4 text-lg">
-        Soy Guillermo, desarrollador web en formación, especializado en front-end y back-end.
-      </p>
+        Soy Guillermo, un desarrollador web en formación con experiencia en front-end y back-end. Tengo un sólido dominio de tecnologías como HTML, CSS, JavaScript, PHP y Python, así como en frameworks como React, Next.js, Angular y Laravel. También tengo habilidades en gestión de bases de datos, trabajando con MySQL y MongoDB, y soy competente en el uso de herramientas de control de versiones como Git.      </p>
       <p className="mt-2 text-lg">
-        Domino tecnologías como HTML, CSS, JavaScript, PHP y Python, así como frameworks como React, Next.js, Angular y Laravel. También manejo bases de datos como MySQL y MongoDB, y tengo experiencia con herramientas de control de versiones como Git. Además, trabajo en entornos Linux, lo que me otorga flexibilidad en la administración de servidores.
-      </p>
-      <p className="mt-2 text-lg">
-        Disfruto colaborar en equipo, aunque me siento cómodo tomando la iniciativa en proyectos independientes. Mi curiosidad por el aprendizaje constante me lleva a mantenerme actualizado con las últimas tendencias y mejores prácticas en el desarrollo web.
-      </p>
+        Mi experiencia en entornos Linux me permite administrar servidores de manera eficiente. Disfruto del trabajo en equipo, aunque también me siento cómodo liderando proyectos de forma independiente. Mi curiosidad y deseo de aprender me mantienen al tanto de las últimas tendencias y mejores prácticas en desarrollo web.      </p>
       <p className="mt-2 text-lg">
         Estoy listo para asumir nuevos retos y contribuir a proyectos innovadores con un impacto real.
       </p>
